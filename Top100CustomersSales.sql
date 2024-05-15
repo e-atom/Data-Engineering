bd58a285fc0992b703fb16174a87c782b30a9795
yuri_bianco <124543632+e-atom@users.noreply.github.com>
2024-05-15 23:17:27 +0900

    Change name

--- Top100CustomersSales.sql
+++ Top100CustomersSales.sql
@@ -1,19 +1,24 @@
 -- USE AdventureWorksDW
 
-/*A Table that represents the top 100 customers which purchases from us */
+/*A Table that represents the Total amount by top 100 customers in the US */
 --
 
 SELECT TOP 100
     CONCAT(c.FirstName, ' ',c.LastName) AS Fullname,
     c.EmailAddress,
+    cu.CurrencyName as Currency,
     SUM(fs.SalesAmount) AS SalesAmount
  FROM DimCustomer c 
  INNER JOIN FactInternetSales fs 
  ON c.CustomerKey = fs.CustomerKey
+ INNER JOIN DimCurrency cu
+ ON fs.CurrencyKey = cu.CurrencyKey
  GROUP BY 
     c.FirstName, 
     c.LastName, 
-    c.EmailAddress
+    c.EmailAddress,
+    cu.CurrencyName
+HAVING cu.CurrencyName = 'US Dollar'
  ORDER BY SalesAmount DESC;
 
 
