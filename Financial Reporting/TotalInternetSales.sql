90da36d04212415a7d2a87d617ed8381b320ae9a
yuri_bianco <124543632+e-atom@users.noreply.github.com>
2024-05-17 18:29:23 +0900

    Adding the script

---
+++ Financial Reporting/TotalInternetSales.sql
@@ -0,0 +1,42 @@
+-- Develop the Total Internet Sales Table
+
+SELECT 
+    fs.SalesOrderNumber AS InvoiceNumber,
+    fs.SalesOrderLineNumber AS InvoiceLineNumber,
+    fs.OrderDate AS OrderDate,
+    fs.SalesAmount AS SalesAmount,
+    
+    dcy.CurrencyName,
+    
+    fs.TaxAmt AS TaxAmount,
+    fs.Freight AS FreightAmount,
+    fs.OrderQuantity AS Quantity,
+
+    dp.EnglishProductName AS ProductName,
+    
+    dpsc.EnglishProductSubcategoryName AS ProductSubCategory,
+    dpc.EnglishProductCategoryName AS ProductCategory,
+    
+    dst.SalesTerritoryCountry,
+    dst.SalesTerritoryRegion,
+    
+    dpy.EnglishPromotionName,
+    dpy.EnglishPromotionType,
+    dpy.EnglishPromotionCategory
+    
+FROM FactInternetSales AS fs
+    INNER JOIN DimProduct AS dp 
+    ON fs.ProductKey = dp.ProductKey
+    INNER JOIN DimProductSubcategory as dpsc
+    ON dp.ProductSubcategoryKey = dpsc.ProductSubcategoryKey
+    INNER JOIN DimProductCategory AS dpc 
+    ON dpsc.ProductCategoryKey = dpc.ProductCategoryKey
+    INNER JOIN DimSalesTerritory AS dst 
+    ON fs.SalesTerritoryKey = dst.SalesTerritoryKey
+    INNER JOIN DimPromotion as dpy
+    ON fs.PromotionKey = dpy.PromotionKey
+    INNER JOIN DimCurrency as dcy 
+    ON fs.CurrencyKey = dcy.CurrencyKey
+
+
+
